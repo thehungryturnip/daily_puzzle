4aad7648564a7e163efa4e98bf85e8a0906502c9
The Hungry Turnip <thehungryturnip@gmail.com>
2022-01-16 22:31:56 -0500

    Implement solver
    The solver uses recursive depth first search to try to place the pieces
    one by one.

--- daily_puzzle.py
+++ daily_puzzle.py
@@ -3,26 +3,9 @@
 import sys
 from collections import defaultdict
 from datetime import date
-from enum import Enum
 
 class Board(defaultdict):
-
-    class Key(Enum):
-        INVALID = ' '
-        OPEN = '_'
-        MONTH = 'M'
-        DAY = 'D'
-        WEEKDAY = 'W'
-        P0 = '0'
-        P1 = '1'
-        P2 = '2'
-        P3 = '3'
-        P4 = '4'
-        P5 = '5'
-        P6 = '6'
-        P7 = '7'
-        P8 = '8'
-        P9 = '9'
+    OPEN = '-'
 
     PIECES = (
         # 0000
@@ -47,13 +30,13 @@ class Board(defaultdict):
         # 333
         #  3
         #  3
-        (((0,0), (0,1), (0,1), (1,1), (2,1)),
+        (((0,0), (0,1), (0,2), (1,1), (2,1)),
          ((0,2), (1,0), (1,1), (1,2), (2,2)), # r90
          ((0,1), (1,1), (2,0), (2,1), (2,2)), # r180
          ((0,0), (1,0), (1,1), (1,2), (2,0))), # r270
         # 444
         # 4 4
-        (((0.0), (0,1), (0,2), (1,0), (1,2)),
+        (((0,0), (0,1), (0,2), (1,0), (1,2)),
          ((0,0), (0,1), (1,1), (2,0), (2,1)), # r90
          ((0,0), (0,2), (1,0), (1,1), (1,2)), # r180
          ((0,0), (0,1), (1,0), (2,0), (2,1))), # r270
@@ -100,45 +83,64 @@ class Board(defaultdict):
          ((0,1), (0,2), (0,3), (1,0), (1,1)), # flip
          ((0,0), (1,0), (1,1), (2,1), (3,1)), # flip r90
          ((0,2), (0,3), (1,0), (1,1), (1,2)), # flip r180
-         ((0,0), (1,0), (2,0), (2,1), (3.1))) # flip r270
+         ((0,0), (1,0), (2,0), (2,1), (3,1))) # flip r270
     )
 
     def __init__(self, today):
-        super(Board, self).__init__(lambda: self.Key.INVALID)
+        super(Board, self).__init__(lambda: ' ')
         self.__init_board()
         self.__set_date(today)
+
+    def solve(self, workingOn = 0):
+        if workingOn == len(self.PIECES):
+            return True
+
+        for permutation in self.PIECES[workingOn]:
+            for r, c in [(r, c) for r in range(8) for c in range(7)]:
+                coords = [(r + dr, c + dc) for (dr, dc) in permutation]
+                if all(self[coord] == self.OPEN for coord in coords):
+                    for coord in coords:
+                        self[coord] = str(workingOn)
+
+                    if self.solve(workingOn + 1):
+                        return True
+
+                    for coord in coords:
+                        self[coord] = str(self.OPEN)
+        return False
 
     def __repr__(self):
         rows = []
         for r in range(8):
             row = []
             for c in range(7):
-                row.append(self[(r, c)].value)
+                row.append(self[(r, c)])
             rows.append(' '.join(row))
         board = '\n'.join(rows)
         return board
 
     def __init_board(self):
         for r, c in [(r, c) for r in range(2) for c in range(6)]:
-                self[(r, c)] = self.Key.OPEN
+            self[(r, c)] = self.OPEN
         for r, c in [(r, c) for r in range(2, 7) for c in range(7)]:
-                self[(r, c)] = self.Key.OPEN
+            self[(r, c)] = self.OPEN
         for c in range(4, 7):
-            self[(7, c)] = self.Key.OPEN
+            self[(7, c)] = self.OPEN
 
     def __set_date(self, today):
-        m = today.month
-        self[(m // 6, m % 6)] = self.Key.MONTH
+        # m is 0-based
+        m = today.month - 1
+        self[(m // 6, m % 6)] = 'M'
 
         # d is 0-based
         d = today.day - 1
-        self[(d // 7 + 2, d % 7)] = self.Key.DAY
+        self[(d // 7 + 2, d % 7)] = 'D'
 
         w = today.weekday()
         if w == 6:
-            self[(6, 3)] = self.Key.WEEKDAY
+            self[(6, 3)] = 'W'
         else:
-            self[(w // 3 + 6, w % 3 + 4)] = self.Key.WEEKDAY
+            self[(w // 3 + 6, w % 3 + 4)] = 'W'
 
 if len(sys.argv) > 1:
     today = date.fromisoformat(sys.argv[1])
@@ -149,4 +151,5 @@ WEEKDAYS = ('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
 print(f'{today} {WEEKDAYS[today.weekday()]}')
 
 board = Board(today)
+board.solve()
 print(board)
